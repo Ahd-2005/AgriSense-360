e3fec2cf9830cd27e0ea8ddcd07374a5e50f0f25
Mohamed Amine Ben Abdeljlil <106981434+AMINbaj124@users.noreply.github.com>
2026-02-15 11:26:05 +0100

    integration

---
+++ src/main/java/utils/MyDataBase.java
@@ -0,0 +1,37 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MyDataBase {
+
+    private static MyDataBase instance;
+    private Connection cnx;
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/agrisense-360";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "";
+
+    private MyDataBase() {
+        try {
+            cnx = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("✅ Connected to database agrisense-360");
+        } catch (SQLException e) {
+            System.out.println("❌ Database connection failed");
+            e.printStackTrace();
+        }
+    }
+
+    public static MyDataBase getInstance() {
+        if (instance == null) {
+            instance = new MyDataBase();
+        }
+        return instance;
+    }
+
+    public Connection getCnx() {
+        return cnx;
+    }
+}
